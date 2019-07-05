d6bb5976d6cd8a48098cbff3742368d4c54b7602
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-05 10:41:51 -0400

    Added second question and score elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,24 @@
+var totalScore = 0;
+
+
+
 
 //---------------GAME SETUP---------------//
 
 $(document).ready(function () {
     console.log("Document Loaded!");
 });
+
+
+// Tell the computer which choices are correct in each question
+
+// When user clicks submit, compare each selection against the correct answers
+
+// If user chose the correct answer, then it's true
+
+    // Add a point to total questions correct counter
+
+// If user chose a wrong answer, it's false
+
+    // Don't add any points
+
