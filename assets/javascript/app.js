c22b1cb09bfef354316b881ca206fbcf7185c209
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-05 10:06:48 -0400

    Set up jquery, JS, and html docs

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//---------------GAME SETUP---------------//
+
+// A $( document ).ready() block.
+// Need this to start doing jQuery commands
+
+$(document).ready(function () {
+    console.log("Document Loaded!");
+});
