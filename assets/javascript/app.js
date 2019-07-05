f77c35972a3fbcc507cc768bacccab16861d53eb
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-05 12:23:38 -0400

    Cleaning up our code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
+// Define all the attributes of the survey object that we'll call upon in our functions
 var surveyQuestions = [
-    {question: "1. What color is the sky?",
-    answers: ["Red", "Green", "Orange", "Blue"],
-    correctAnswer: "Blue"},
+    {
+        question: "1. What color is the sky?",
+        answers: ["Red", "Green", "Orange", "Blue"],
+        correctAnswer: "Blue"
+    },
 
-    {question: "2. What shape is the earth?",
-    answers: ["Square", "Round", "Triangle", "Flat"],
-    correctAnswer: "Round"}
+    {
+        question: "2. What shape is the earth?",
+        answers: ["Square", "Round", "Triangle", "Flat"],
+        correctAnswer: "Round"
+    }
 
 ];
-console.log(surveyQuestions[0].question)
-
-
 //---------------GAME SETUP---------------//
 
-// Define all the global variables here that will run the game 
+// Define all the global variables here that will run the game
 // Always end stuff unside an objet with a comma, instead of semicolon
 // This is called a constructor 
 // We declared methods and properties in this survey object
@@ -22,24 +24,31 @@ var survey = {
     correctAnswers: 0,
     incorrectAnswers: 0,
     questions: surveyQuestions,
+
     // Function that puts the questions on the page 
     // When the page loads, the user will get a message that tells what the survey is about 
     // When user clicks start button, questions will dynamically appear on the page
-
     // Loop through the answers and grab them dynamically through the array
-    showQuestions: function() {
+    showQuestions: function () {
         for (i = 0; i < survey.questions.length; i++) {
             console.log(survey.questions[i]);
-            // Show question inside of survey and concat
-            // "." is how we move through all the objects in our array (Opening the next level in our object)
+
+            // Show questions inside of survey and concatenate everything together
+            // "." is how we open the next level and move through all the objects in our array
             $(".questions-text").append("<p><strong>" + survey.questions[i].question + "</strong></p>")
             for (j = 0; j < survey.questions[i].answers.length; j++) {
                 $(".questions-text").append("<p>" + survey.questions[i].answers[j] + "</p>")
+                // We need to insert radio buttons next to each answer in the HTML from our JS document
+
+                // <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                // <label class="custom-control-label" for="customRadioInline1">Red</label>
             }
-        } 
+        } console.log(survey.questions)
     }
 }
-    // console.log(survey.questions)
+
+//---------------START GAME---------------//
+
 $("#start").on("click", function () {
     survey.showQuestions();
 });
