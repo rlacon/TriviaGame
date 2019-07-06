6c2837dc9ee618d3cf587a2bbb68e515bb36b791
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-06 15:45:51 -0400

    Added radio buttons before survey questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,30 +29,40 @@ var survey = {
     // When the page loads, the user will get a message that tells what the survey is about
     // When user clicks start button, questions will dynamically appear on the page
     // Loop through the answers and grab them dynamically through the array
+
     showQuestions: function () {
         for (i = 0; i < survey.questions.length; i++) {
             console.log(survey.questions[i]);
+
 
             // Show questions inside of survey and concatenate everything together
             // "." is how we open the next level and move through all the objects in our array
             $(".questions-text").append("<p><strong>" + survey.questions[i].question + "</strong></p>")
             for (j = 0; j < survey.questions[i].answers.length; j++) {
+                // console.log("Current Question is : " + survey.questions[i]);
+                // console.log("Current Option is : " + survey.questions[i]);
+                $(".questions-text").append('<div id="option_' + i + '' + j + '"></div>');
                 $(".questions-text").append("<p>" + survey.questions[i].answers[j] + "</p>")
                 // We need to insert radio buttons next to each answer in the HTML from our JS document
 
-                // <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                var radioBtn = $('<input type="radio" name="rbtnCount" />');
+                radioBtn.appendTo('#option_' + i + '' + j);
+
                 // <label class="custom-control-label" for="customRadioInline1">Red</label>
+                //     < input type = "radio" id = "customRadioInline1" name = "customRadioInline1" class= "custom-control-input" > 
+                
             }
         } console.log(survey.questions)
     }
 }
 
 //---------------START GAME---------------//
+
 $("#submit").hide();
 $(".scoreCounter").hide();
 $("#start").on("click", function () {
     survey.showQuestions();
     $("#start").hide();
     $("#submit").show();
-    $("#trivia-instructions-text").hide();
+    $("#instructions-text").hide();
 });
